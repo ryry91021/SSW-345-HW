4eb7cb7e0c006603b80dca20e84aeb5cf47f8299
ryry91021 <ryry91021@gmail.com>
2024-12-03 15:01:00 -0500

    finished parts 1 and 2 of singletonvisitorpatterns hw

---
+++ singletonVisitorPattern/src/part2/Mouse.java
@@ -0,0 +1,9 @@
+package part2;
+
+public class Mouse implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+    }
+}
+
